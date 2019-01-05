a06c359e252869053f27de6e30f3fefd02bef562
Iosi Ginerman <iginerman@hotmail.com>
2019-01-06 00:03:56 +0200

    Added constants file

---
+++ Infrastructure/TriviaConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class TriviaConstants
+    {
+        public readonly string True = "true";
+        public readonly string False = "false";
+
+    }
+}
