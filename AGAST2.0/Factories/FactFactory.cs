76a710fa9420eb36f3cb9b84d6cf4cadbdf0f4d6
Iosi Ginerman <iginerman@hotmail.com>
2019-01-05 23:15:28 +0200

    Added basic factories

---
+++ AGAST2.0/Factories/FactFactory.cs
@@ -0,0 +1,20 @@
+﻿
+using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AGAST2.Factories
+{
+    public class FactFactory : IFactory<bool>
+    {
+        private List<string> _askedFacts;
+
+        public List<string> AskedFacts { get => _askedFacts; set => _askedFacts = value; }
+
+        public ILevel<bool> NextLevel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AGAST2.0/Factories/QuestionFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AGAST2.Factories
+{
+    public class QuestionFactory
+    {
+        private List<string> _askedQuestions;
+
+        public List<string> AskedQuestions { get => _askedQuestions; set => _askedQuestions = value; }
+    }
+}
