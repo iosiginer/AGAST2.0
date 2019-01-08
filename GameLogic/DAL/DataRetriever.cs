e109e72c486ae09806e688452db497d97e032711
Uri <ronenuri@gmail.com>
2019-01-08 17:43:04 +0200

    Added a DBManager

--- GameLogic/DAL/DataRetriever.cs
+++ GameLogic/DAL/DataRetriever.cs
@@ -2,13 +2,35 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
-namespace AGAST2.GameLogic
+namespace AGAST2.GameLogic.DAL
 {
     public class DataRetriever : IDataRetriever
     {
+        private MySqlConnection connection;
+        private string connectionString;
         private Random randy;
 
+
+        private DataRetriever()
+        {
+            this.Initialize();
+        }
+
+        private void Initialize()
+        {
+            connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString;
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+            
+        }
+
+        public string GetFact()
+        {
+            connection.
+        }
 
         public ArtistData GetArtistDataFromName(string artist)
         {
